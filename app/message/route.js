3f5c95ea6187006f03c5b88305ba0bf5a6792402
rouz majlessi <rmajlessi@gmail.com>
2017-03-07 02:48:18 -0800

    add action and return model in routes messages

--- app/message/route.js
+++ app/message/route.js
@@ -1,4 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  userFromParams: null,
+
+  model(params) {
+    this.set('userFromParams', params.user_name);
+    return this.store.findAll('messages');
+  },
+
+
+  actions: {
+    createMessage(messages) {
+      let newRecord = this.store.createRecord('messages', {
+        text: messages,
+        user:'Rouz'
+      });
+
+      newRecord.save();
+
+      this.controller.set('textMessageFromInput', '');
+    }
+  }
 });
