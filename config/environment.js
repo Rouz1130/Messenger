2ba08b4d3ee37c84f9dd189a17762e33ce6b590a
rouz majlessi <rmajlessi@gmail.com>
2017-03-07 02:13:03 -0800

    add firebase

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'messenger',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://meetup-chat-app.firebaseio.com/',
     rootURL: '/',
     locationType: 'auto',
     EmberENV: {
